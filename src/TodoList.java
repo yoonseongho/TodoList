ffdb132c2d70be4df0ea8c9a110f0d2649753848
yoonseongho <sxdz1290@naver.com>
2023-03-14 14:18:12 +0900

    TodoList

--- src/TodoList.java
+++ src/TodoList.java
@@ -3,154 +3,155 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TodoList extends JFrame implements ActionListener {
-    private JPanel contentPane;
-    private JTextField taskField;
-    private JTextField dateField;
+
+    private JTextField taskInput;
+    private JTextArea taskList;
     private JButton addButton;
-    private JButton editButton;
     private JButton deleteButton;
-    private JList<String> list;
-    private DefaultListModel<String> listModel;
+    private JButton updateButton;
+    private JButton saveButton;
+    private List<String> tasks;
 
     public TodoList() {
-        setTitle("Todo List");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 400, 500);
+        super("Todo List");
 
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new BorderLayout(0, 0));
-        setContentPane(contentPane);
+        tasks = new ArrayList<>();
 
-        JPanel inputPanel = new JPanel(new GridLayout(0, 2, 5, 5));
-        contentPane.add(inputPanel, BorderLayout.NORTH);
+        // Main Panel
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        // Task Input Panel
+        JPanel taskInputPanel = new JPanel(new BorderLayout());
+        taskInputPanel.setBorder(new EmptyBorder(0, 0, 10, 0));
 
         JLabel taskLabel = new JLabel("Task:");
-        inputPanel.add(taskLabel);
-
-        taskField = new JTextField();
-        inputPanel.add(taskField);
-        taskField.setColumns(10);
-
-        JLabel dateLabel = new JLabel("Date (MM/dd/yyyy):");
-        inputPanel.add(dateLabel);
-
-        dateField = new JTextField();
-        inputPanel.add(dateField);
-        dateField.setColumns(10);
-
-        // 추가
+        taskInput = new JTextField(20);
         addButton = new JButton("Add");
         addButton.addActionListener(this);
-        inputPanel.add(addButton);
+        taskInputPanel.add(taskLabel, BorderLayout.WEST);
+        taskInputPanel.add(taskInput, BorderLayout.CENTER);
+        taskInputPanel.add(addButton, BorderLayout.EAST);
 
-        // 수정
-        editButton = new JButton("Edit");
-        editButton.addActionListener(this);
-        editButton.setEnabled(false);
-        inputPanel.add(editButton);
+        // Task List Panel
+        JPanel taskListPanel = new JPanel(new BorderLayout());
+        taskListPanel.setBorder(new EmptyBorder(10, 0, 0, 0));
 
-        // 삭제
+        JLabel taskListLabel = new JLabel("Tasks:");
+        taskList = new JTextArea();
+        taskList.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(taskList);
+        scrollPane.setPreferredSize(new Dimension(0, 200));
+        taskListPanel.add(taskListLabel, BorderLayout.NORTH);
+        taskListPanel.add(scrollPane, BorderLayout.CENTER);
+
+        // Button Panel
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         deleteButton = new JButton("Delete");
         deleteButton.addActionListener(this);
-        deleteButton.setEnabled(false);
-        inputPanel.add(deleteButton);
+        updateButton = new JButton("Update");
+        updateButton.addActionListener(this);
+        saveButton = new JButton("Save");
+        saveButton.addActionListener(this);
+        buttonPanel.add(deleteButton);
+        buttonPanel.add(updateButton);
+        buttonPanel.add(saveButton);
 
-        listModel = new DefaultListModel<>();
-        list = new JList<>(listModel);
-        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        list.addListSelectionListener(e -> {
-            if (list.getSelectedIndex() == -1) {
-                editButton.setEnabled(false);
-                deleteButton.setEnabled(false);
-            } else {
-                editButton.setEnabled(true);
-                deleteButton.setEnabled(true);
-                String[] selected = list.getSelectedValue().split(" - ");
-                taskField.setText(selected[0]);
-                dateField.setText(selected[1]);
-            }
-        });
-        JScrollPane scrollPane = new JScrollPane(list);
-        contentPane.add(scrollPane, BorderLayout.CENTER);
+        // Add Panels to Main Panel
+        mainPanel.add(taskInputPanel, BorderLayout.NORTH);
+        mainPanel.add(taskListPanel, BorderLayout.CENTER);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+
+        // Add Main Panel to Frame
+        add(mainPanel);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 400);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                    TodoList frame = new TodoList();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        new TodoList();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == addButton) {
-            String task = taskField.getText();
-            String dateString = dateField.getText();
-            if (!task.isEmpty() && !dateString.isEmpty()) {
-                try {
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-                    Date date = dateFormat.parse(dateString);
-                    String formattedDate = dateFormat.format(date);
-                    listModel.addElement(task + " - " + formattedDate);
-                    taskField.setText("");
-                    dateField.setText("");
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(this, "Invalid date format. Please use MM/dd/yyyy format.", "Error", JOptionPane.ERROR_MESSAGE);
-                }
-            } else {
-                JOptionPane.showMessageDialog(this, "Please fill out both fields.", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        }else if (e.getSource() == editButton) {
-            int selectedIndex = list.getSelectedIndex();
-            String task = taskField.getText();
-            String dateString = dateField.getText();
-            if (!task.isEmpty() && !dateString.isEmpty()) {
-                try {
-                    SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-                    Date date = dateFormat.parse(dateString);
-                    String formattedDate = dateFormat.format(date);
-                    listModel.set(selectedIndex, task + " - " + formattedDate);
-                    taskField.setText("");
-                    dateField.setText("");
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(this, "Invalid date format. Please use MM/dd/yyyy format.", "Error", JOptionPane.ERROR_MESSAGE);
-                }
-            } else {
-                JOptionPane.showMessageDialog(this, "Please fill out both fields.", "Error", JOptionPane.ERROR_MESSAGE);
-            }
+            addTask();
         } else if (e.getSource() == deleteButton) {
-            int selectedIndex = list.getSelectedIndex();
-            if (selectedIndex != -1) {
-                int confirmed = JOptionPane.showConfirmDialog(this, "Are you sure you want to delete this item?", "Confirm", JOptionPane.YES_NO_OPTION);
-                if (confirmed == JOptionPane.YES_OPTION) {
-                    listModel.remove(selectedIndex);
-                    taskField.setText("");
-                    dateField.setText("");
-                    editButton.setEnabled(false);
-                    deleteButton.setEnabled(false);
-                }
+            deleteTask();
+        } else if (e.getSource() == updateButton) {
+            updateTask();
+        } else if (e.getSource() == saveButton) {
+            saveTasks();
+        }
+    }
+
+    private void addTask() {
+        String task = taskInput.getText();
+        if (task != null && !task.trim().isEmpty()) {
+            tasks.add(task);
+            updateTaskList();
+            taskInput.setText("");
+        }
+    }
+    private void deleteTask() {
+        int start = taskList.getSelectionStart();
+        int end = taskList.getSelectionEnd();
+        int index = taskList.getText().substring(0, end).split("\n").length - 1;
+
+        if (index != -1) {
+            tasks.remove(index);
+            updateTaskList();
+        }
+    }
+
+    private void updateTask() {
+        int start = taskList.getSelectionStart();
+        int end = taskList.getSelectionEnd();
+        int index = taskList.getText().substring(0, end).split("\n").length - 1;
+
+        if (index != -1) {
+            String newTask = JOptionPane.showInputDialog(this, "Enter new task:", tasks.get(index));
+            if (newTask != null && !newTask.trim().isEmpty()) {
+                tasks.set(index, newTask);
+                updateTaskList();
             }
         }
     }
 
-    /* public void showTodoList() {
-        ArrayList<String> todoList = new ArrayList<>();
-        todoList.add("Complete math homework - 03/14/2023");
-        todoList.add("Buy groceries - 03/15/2023");
-        todoList.add("Go for a walk - 03/16/2023");
-        for (String item : todoList) {
-            listModel.addElement(item);
+    private void saveTasks() {
+        JFileChooser fileChooser = new JFileChooser();
+        int result = fileChooser.showSaveDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileChooser.getSelectedFile()))) {
+                for (String task : tasks) {
+                    LocalDateTime now = LocalDateTime.now();
+                    String date = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+                    writer.write(String.format("%s\n%s\n", date, task));
+                }
+                JOptionPane.showMessageDialog(this, "Tasks saved successfully!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    } */
+    }
+
+    private void updateTaskList() {
+        taskList.setText("");
+        for (String task : tasks) {
+            taskList.append(task + "\n");
+        }
+    }
 }
